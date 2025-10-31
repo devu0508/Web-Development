0f68566a16e2cfd5a055292cf15c931f38f31b7d
devu0508 <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-01 01:58:05 +0530

    Day14

---
+++ Javascript/Day14/first.js
@@ -0,0 +1,56 @@
+// console.log("first");
+// let sum = 0;
+// for(let i=0; i<3000000000;i++){
+//     sum+=i;
+
+//     console.log(sum);
+//     console.log("Last");
+// };
+
+// console.log("Hello Ji");
+
+// setTimeout(()=>{
+//     console.log("Time Out Executed");
+// },5000);
+
+// console.log("I am the end");
+
+// console.log("Hi Ji");
+// const button1 = document.getElementById("button1");
+// button1.addEventListener('click',()=>{
+//     console.log("Button1 is clicked");
+// });
+
+// const button2 = document.getElementById("button2");
+// button2.addEventListener('click',()=>{
+//     console.log("Button2 is clicked");
+// });
+
+// const button3 = document.getElementById("button3");
+// button3.addEventListener('click',()=>{
+//     console.log("Button3 is clicked");
+// });
+
+// console.log("I am the end");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
