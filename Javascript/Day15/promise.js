8f8b59b8206468f6b021ec1b7877ef01d143dbf3
Divyansh <divyanshupadhyay23aiml@rnsit.ac.in>
2025-11-06 22:54:50 +0530

    promise

---
+++ Javascript/Day15/promise.js
@@ -0,0 +1,29 @@
+console.log('This is Promises');
+
+let prom1 = new Promise((resolve , reject)=>{
+    let a = Math.random();
+    if(a<0.5){
+        reject("No random number was not supporting you")
+    }
+    else{
+
+      setTimeout(() => {
+         console.log("Yes I am done")
+         resolve("Harry")
+       },3000);  
+    }
+       
+})
+
+prom1.then((a)=>{
+    console.log(a)
+}).catch((error)=>{
+    console.log(error)
+})
+
+
+
+
+
+
+
